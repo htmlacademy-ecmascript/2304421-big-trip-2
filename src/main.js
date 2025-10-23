c065041d8cb95d46e8605428e19cd29cc6390407
Sergey Kozhevnikov <sergeykog01@gmail.com>
2025-10-23 21:14:52 +0300

    Add sorting module

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import NewFilterView from './view/filters.js';
+import NewSortingView from './view/sorting.js';
 import { render } from './render.js';
 
 const siteHeaderElement = document.querySelector('.page-header');
 const filtersElement = siteHeaderElement.querySelector('.trip-controls__filters');
+const mainElement = document.querySelector('.page-main');
+const tripEventsElement = mainElement.querySelector('.trip-events');
 
 render(new NewFilterView(), filtersElement);
+render(new NewSortingView(), tripEventsElement);
