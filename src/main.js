26ce41eb79e35f4d822dd5d5f0a22fcf80af8d8f
Sergey Kozhevnikov <sergeykog01@gmail.com>
2025-10-26 19:53:40 +0300

    Add route-points module

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import NewFilterView from './view/filters.js';
 import NewSortingView from './view/sorting.js';
+import NewPointView from './view/route-point.js';
 import { render } from './render.js';
 
 const siteHeaderElement = document.querySelector('.page-header');
 const filtersElement = siteHeaderElement.querySelector('.trip-controls__filters');
 const mainElement = document.querySelector('.page-main');
 const tripEventsElement = mainElement.querySelector('.trip-events');
+const tripMainElement = siteHeaderElement.querySelector('.trip-main');
 
 render(new NewFilterView(), filtersElement);
 render(new NewSortingView(), tripEventsElement);
+render(new NewPointView(), tripMainElement, 'afterbegin');
