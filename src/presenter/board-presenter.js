66c652a519c205a07d5ad41f0ac3c0f45499f4ca
Sergey Kozhevnikov <sergeykog01@gmail.com>
2025-10-30 15:05:51 +0300

    Add new point module

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,6 +1,7 @@
 import SortingView from '../view/sorting.js';
 import TripEventsListView from '../view/trip-events-list.js';
 import NewRoutePoint from '../view/route-point.js';
+import NewPoint from '../view/add-new-point.js';
 import { render } from '../render.js';
 
 export default class BoardPresenter {
@@ -13,6 +14,10 @@ export default class BoardPresenter {
   init() {
     render(new SortingView(), this.boardContainer);
     render(this.boardComponent, this.boardContainer);
-    render(new NewRoutePoint(), this.boardComponent.getElement());
+    render(new NewPoint(), this.boardComponent.getElement());
+
+    for (let i = 0; i < 3; i++) {
+      render(new NewRoutePoint(), this.boardComponent.getElement());
+    }
   }
 }
