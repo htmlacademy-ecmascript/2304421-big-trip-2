0136bd35144f9d516dbdcfa89d908c391c4221bc
Sergey Kozhevnikov <sergeykog01@gmail.com>
2026-01-18 22:03:23 +0300

    Edits update point methos

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -43,20 +43,27 @@ export default class PointsModel extends Observable {
     this._notify(UpdateType.INIT);
   }
 
-  updatePoint(updateType, update) {
-    const index = this.#points.findIndex((point) => point.id === update.id);
+  async updatePoint(updateType, update) {
 
-    if (index === -1) {
-      throw new Error('Can\'t update unexisting point');
+    try {
+      const response = await this.#pointsApiService.updatePoint(update);
+      const updatedPoint = this.#adaptToClient(response);
+
+      const index = this.#points.findIndex((point) => point.id === update.id);
+
+      if (index === -1) {
+        throw new Error('Can\'t update unexisting point');
+      }
+
+      this.#points = [
+        ...this.#points.slice(0, index),
+        updatedPoint,
+        ...this.#points.slice(index + 1),
+      ];
+      this._notify(updateType, updatedPoint);
+    } catch(err) {
+      throw new Error('Cant\'t update point');
     }
-
-    this.#points = [
-      ...this.#points.slice(0, index),
-      update,
-      ...this.#points.slice(index + 1),
-    ];
-
-    this._notify(updateType, update);
   }
 
   addPoint(updateType, update) {
